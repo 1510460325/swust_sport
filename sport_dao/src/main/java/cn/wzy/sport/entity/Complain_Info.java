373e73f6124871ab0e3918c99a0fa5d422470ee7
1510460325@qq.com <1510460325@qq.com>
2018-07-16 18:21:12 +0800

    更新依赖项

--- sport_dao/src/main/java/cn/wzy/sport/entity/Complain_Info.java
+++ sport_dao/src/main/java/cn/wzy/sport/entity/Complain_Info.java
@@ -1,8 +1,6 @@
 package cn.wzy.sport.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.util.Date;
